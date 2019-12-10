606fedb43d76a1377901e38fc552c698a903d5db
shinseunghyun <akasiassh@naver.com>
2019-12-10 20:26:12 +0900

    내려받기

--- src/App.js
+++ src/App.js
@@ -1,10 +1,65 @@
 import React, { Component } from 'react';
-import dvTest from './dvtest'
+import axios from 'axios';
+
+import './App.css';
+
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Main from './components/Main';
+import Navi from './components/Navi';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      clickedDB: 'korea',
+      clickedSubMenu: 'DataLoad'
+    };
+  }
+
+  componentDidMount() {
+  }
+
+  ChangeDB = (_clickedDB) => {
+    this.setState({
+      clickedDB: _clickedDB,
+      clickedSubMenu: 'DataLoad'
+    });
+  }
+
+  ChangeSubMenu = (_clickedSubMenu) => {
+    this.setState({
+      clickedSubMenu: _clickedSubMenu
+    });
+  }
+
+  _test = async() => {
+    await axios({
+      url: '/test',
+      method: 'POST'
+    }).then(response => response.data).then((data) => {
+      alert(data);
+    });
+  }
+
   render() {
-    return (
-      <dvTest></dvTest>
+    return(
+      <div className = "App">
+        <div id = "top">
+          <Header ChangeDB = {this.ChangeDB}></Header>
+        </div>
+        <div id = "middle">
+          <div id = "Navi">
+            <Navi clickedDB = {this.state.clickedDB} ChangeSubMenu = {this.ChangeSubMenu}></Navi>
+          </div>
+          <div id = "Main">
+            <Main clickedSubMenu = {this.state.clickedSubMenu}></Main>
+          </div>
+        </div>
+        <div id = "bottom">
+          <Footer></Footer>
+        </div>
+      </div>
     )
   }
 }
