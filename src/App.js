d860c5e670bd036dc5322cc8537cd67c5f19a21f
shinseunghyun <akasiassh@naver.com>
2019-12-10 20:21:14 +0900

    내려받기

--- src/App.js
+++ src/App.js
@@ -1,29 +1,10 @@
-import React, {Component} from 'react';
-import './App.css';
-import axios from 'axios'
+import React, { Component } from 'react';
+import dvTest from './dvtest'
 
 class App extends Component {
-  constructor(props){
-    super(props)
-    this.state = {
-      host : '',
-    }
-  }
-
-  componentDidMount() {
-    this._getHost();
-  }
-
-  _getHost = async() => {
-    const res = await axios.get('/api/host');
-    this.setState({host : res.data.host})
-  }
-
-  render(){
-    return(
-      <div className = 'App'>
-        <h3>Welcome to <u>{this.state.host}</u> Blog!</h3>
-      </div>
+  render() {
+    return (
+      <dvTest></dvTest>
     )
   }
 }
---
+++ src/dvtest.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class dvTest extends Component {
+    render() {
+        return (
+            <p>heelo!</p>
+        );
+    }
+}
+
+export default dvTest;
