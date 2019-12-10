8dd19dc272bdb85be7ad49115dcfadf0aff8b0c6
shinseunghyun <akasiassh@naver.com>
2019-12-10 20:52:41 +0900

    내려받기

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import * as d3 from 'd3'
 
 class Main extends Component {
     constructor(props) {
@@ -8,11 +9,21 @@ class Main extends Component {
     }
 
     componentDidMount() {
+        var svg = d3.select(".aaaa")
+            .append("svg")
+            .attr("width", 500)
+            .attr("height", 500)
+
+        svg.append("rect")
+            .attr('x', 10)
+            .attr('y', 10)
+            .attr('width', 10)
+            .attr('height', 10)
     }
 
     render() {
         return (
-            <div>
+            <div className = 'aaaa'>
                 {this.props.clickedSubMenu}
             </div>
         );
